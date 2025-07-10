b9e34160fcae1dc5afc278e73a8cb4ea50eccb2a
PoojaDev74 <poojapreethi2001@gmail.com>
2025-07-10 21:15:55 +0530

    push folder

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Records from '../../Components/Records/Records'
+import "./HomeStyle.css"
+const Home = () => {
+  return (
+ <>
+<section className='home-section'>
+<Records/>
+
+</section>
+   
+ </>
+  )
+}
+
+export default Home
