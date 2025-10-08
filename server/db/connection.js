15d8da32fbdb78b9604f5e0d79902cc67adfac21
Pooja D <poojapreethi2001@gmail.com>
2025-10-08 13:34:13 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("connection established");
 })
